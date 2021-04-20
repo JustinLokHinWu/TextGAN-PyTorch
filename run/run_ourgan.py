7abcefa7481ea9ade8ddc2a765f0a52ce3fca190
Justin Wu <justin4827@gmail.com>
2021-04-19 23:44:03 -0400

    Test

--- run/run_ourgan.py
+++ run/run_ourgan.py
@@ -38,7 +38,7 @@ CUDA = int(True)
 oracle_pretrain = int(True)
 gen_pretrain = int(False)
 dis_pretrain = int(False)
-MLE_train_epoch = 20
+MLE_train_epoch = 10 # TODO undo
 ADV_train_epoch = 200
 tips = 'OurGAN experiments'
 
