02f6d4dfe8f7e0522478efe3c7330ec5efee2b26
Justin Wu <justin4827@gmail.com>
2021-04-20 01:18:24 -0400

    Readd pos encoding

--- models/OurGAN_D.py
+++ models/OurGAN_D.py
@@ -72,7 +72,9 @@ class OurGAN_D(nn.Module):
         encodings[:, 1::2] = cos_vals
 
         # Seq len x Embedding dimension
-        encodings = encodings.cuda()
+        if self.gpu:
+            encodings = encodings.cuda()
+        encodings = torch.Variable(encodings)
 
         return encodings
 
@@ -85,7 +87,7 @@ class OurGAN_D(nn.Module):
         emb = self.embeddings(inp) # batch_size * max_seq_len * embed_dim
 
         seqlen = inp.size(1)
-        # emb = emb + self.pos_encoding[:seqlen]
+        emb = emb + self.pos_encoding[:seqlen]
 
         trans = self.transformer(emb) # batch * max_seq_len * embed_dim
 
